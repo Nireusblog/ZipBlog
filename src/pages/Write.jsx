0b1b8c34b2fa54b52c4bc6b3f9cdd0ae65a4992c
dayvidtran <dayvidtran@gmail.com>
2023-05-11 23:23:39 -0400

    new post text with react-quill. ability to add tag to new post

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const Write = () => {
+  const [value, setValue] = useState('');
+
+  console.log(value)
   return (
     <div className='add' >
       <div className="content">
-        <input type="text" placeholder='title'/>
+        <input type="text" placeholder='Title'/>
+        <div className="editorcontainer">
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        
+        </div>
       </div>
     
       <div className="menu">
-        <div className="item">i1</div>
-        <div className="item">i2</div>
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style ={{display: "none"}} type ="file" id="file" name="" />
+          <label htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as a Draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Tag</h1>
+          <input type="radio" name="cat"  value="art" id = "art"/>
+          <label htmlFor="art">Art</label>
+          <input type="radio" name="cat"  value="science" id = "science"/>
+          <label htmlFor="science">Science</label>
+          <input type="radio" name="cat"  value="technology" id = "technology"/>
+          <label htmlFor="technology">Technology</label>
+          <input type="radio" name="cat"  value="cinema" id = "cinema"/>
+          <label htmlFor="cinema">Cinema</label>
+          <input type="radio" name="cat"  value="food" id = "food"/>
+          <label htmlFor="food">Food</label>
+        </div>
       </div>
     </div>
   )
