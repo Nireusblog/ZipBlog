4b9a16b7769b3aa23c06ce4ca259ee68b713b162
dayvidtran <dayvidtran@gmail.com>
2023-05-13 15:44:05 -0400

    logout link isnt working. got homepage to connect to database so whatever is stored in database underpost will generate on homepage. example with post 1

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,4 +1,27 @@
+import { db } from "../db.js";
+
+export const getPosts =(req,res)=>{
+    const q = req.query.tag
+    ? "SELECT * FROM posts WHERE tag=?"
+     :"SELECT * FROM posts";
+
+     db.query(q, [req.query.tag], (err,data)=>{
+        if (err) return res.send(err)
+
+        return res.status(200).json(data);
+
+     })
+};
+export const getPost =(req,res)=>{
+    res.json("from controller")
+}
 export const addPost =(req,res)=>{
+    res.json("from controller")
+}
+export const deletePost =(req,res)=>{
+    res.json("from controller")
+}
+export const updatePost =(req,res)=>{
     res.json("from controller")
 }
 
