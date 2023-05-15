f73858c28c4095ceb8bb75045784201fc5db6735
ule270 <u.le270@gmail.com>
2023-05-15 14:52:59 -0400

    editing posts now fixed

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -80,7 +80,7 @@ export const updatePost =(req,res)=>{
         if(err) return res.status(403).json("Token is not valid!");
     
     const postId = req.params.id;
-    const q = "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `tag`=?) WHERE `id` = ? AND `uid` = ? "; 
+    const q = "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `tag`=? WHERE `id` = ? AND `uid` = ? "; 
 
     const values = [
         req.body.title,
