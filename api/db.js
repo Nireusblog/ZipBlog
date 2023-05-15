5ccbed1ba642296f85bbab2581ab69448ac1775b
deanOfWalls <deanofwalls@gmail.com>
2023-05-15 01:25:25 -0400

    adding comments

--- api/db.js
+++ api/db.js
@@ -1,8 +1,9 @@
 import mysql from "mysql"
 
+// Create a MySQL connection
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "zipcode0",
-    database: "blog"
+    host: "localhost", // Hostname or IP address of the MySQL server
+    user: "root", // Username for authentication
+    password: "zipcode0", // Password for authentication
+    database: "blog" // Name of the MySQL database to connect to
 })
