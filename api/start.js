083e2ab175be313ba29de262a6d74e4d0680f622
deanOfWalls <deanofwalls@gmail.com>
2023-05-15 01:42:49 -0400

    adding comments

--- api/start.js
+++ api/start.js
@@ -1,13 +1,13 @@
-const { execSync } = require('child_process');
-const path = require('path');
+const { execSync } = require('child_process'); // Import the execSync function from the child_process module
+const path = require('path'); // Import the path module
 
-const startScript = path.join(__dirname, 'package.json');
+const startScript = path.join(__dirname, 'package.json'); // Get the path to the package.json file
 
 try {
-  execSync(`npm start`, {
-    cwd: path.dirname(startScript),
-    stdio: 'inherit',
+  execSync(`npm start`, { // Execute the command "npm start" synchronously
+    cwd: path.dirname(startScript), // Set the current working directory for the command execution
+    stdio: 'inherit', // Inherit the standard input/output streams for the command
   });
 } catch (error) {
-  console.error('Failed to start application:', error);
+  console.error('Failed to start application:', error); // Display an error message if the command execution fails
 }
